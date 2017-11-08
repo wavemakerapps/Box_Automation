fa2d1ab19c41858a5bcaf7b478d20232abed4a55
apptest <appTest1@wavemaker.com>
2017-11-08 08:04:26 +0000

    '; New files added - [BoxOAuth] Files modified - [Main]

---
+++ src/main/webapp/WEB-INF/prefabs/BoxOAuth/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "BoxOAuth",
+  "homePage" : "Main",
+  "name" : "BoxOAuth",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.01"
+};
